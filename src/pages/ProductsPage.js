642491b83a04a46b2ade92da4984422c5a180438
Devangi Bhutiya <devangibhutiya@gmail.com>
2023-06-06 18:17:42 +0530

    added loading and alert0

--- src/pages/ProductsPage.js
+++ src/pages/ProductsPage.js
@@ -1,28 +1,16 @@
-
 import axios from 'axios';
 import { Helmet } from 'react-helmet-async';
 import { useContext, useEffect, useState } from 'react';
- // Adjust the file path accordingly
-
-
-// @mui
-import { Container, Stack, Typography } from '@mui/material';
+import { Container, Stack, Typography, CircularProgress } from '@mui/material';
 import { MainContext } from '../context/MainContext';
-// components
 import { ProductSort, ProductList, ProductCartWidget, ProductFilterSidebar } from '../sections/@dashboard/products';
-// mock
 import PRODUCTS from '../_mock/products';
 
-
-// ----------------------------------------------------------------------
-
-
 export default function ProductsPage() {
-
-const {mainState, setMainState} = useContext(MainContext)
-
-  const encodedToken = localStorage.getItem("token");
+  const { mainState, setMainState } = useContext(MainContext);
+  const encodedToken = localStorage.getItem('token');
   const [openFilter, setOpenFilter] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
   const handleOpenFilter = () => {
     setOpenFilter(true);
@@ -32,52 +20,75 @@ const {mainState, setMainState} = useContext(MainContext)
     setOpenFilter(false);
   };
 
-  
-  useEffect(()=> {
-    const fecthProduct = async () => {
+  useEffect(() => {
+    const fetchProducts = async () => {
       try {
-        const response = await axios.get(`/api/products`, {
+        const response = await axios.get('/api/products', {
           headers: {
-            authorization: encodedToken, // passing token as an authorization header
+            authorization: encodedToken,
           },
-
         });
 
-        setMainState({...mainState,productData:response.data.products,storeOriginalProductData:response.data.products})
+        setMainState({
+          ...mainState,
+          productData: response.data.products,
+          storeOriginalProductData: response.data.products,
+        });
         
-  console.log(response.data.products)
+        setIsLoading(false);
       } catch (error) {
         console.log(error);
       }
+    };
 
-    }
-    fecthProduct();
-  },[])
+    fetchProducts();
+  }, []);
+
   return (
     <>
       <Helmet>
-        
-        <title> Dashboard: Products | Minimal UI </title>
+        <title>Dashboard: Products | Minimal UI</title>
       </Helmet>
 
-      <Container> 
+      <Container>
         <Typography variant="h4" sx={{ mb: 5 }}>
           Products
         </Typography>
 
-        <Stack direction="row" flexWrap="wrap-reverse" alignItems="center" justifyContent="flex-end" sx={{ mb: 5 }}>
+        <Stack
+          direction="row"
+          flexWrap="wrap-reverse"
+          alignItems="center"
+          justifyContent="flex-end"
+          sx={{ mb: 5 }}
+        >
           <Stack direction="row" spacing={1} flexShrink={0} sx={{ my: 1 }}>
             <ProductFilterSidebar
               openFilter={openFilter}
               onOpenFilter={handleOpenFilter}
               onCloseFilter={handleCloseFilter}
             />
-            
+            {/* Add other filter components here */}
           </Stack>
         </Stack>
 
-        <ProductList products={mainState.productData} />
-        <ProductCartWidget />
+        {isLoading ? (
+         <Stack
+         sx={{
+           height: '100vh',
+           alignItems: 'center',
+           justifyContent: 'center',
+         }}
+       >
+        <CircularProgress size={80} />
+       </Stack>
+     ): (
+          <>
+            <ProductSort />
+            <ProductList products={mainState.productData} />
+            <ProductCartWidget />
+          </>
+        )}
       </Container>
     </>
   );
--- src/sections/@dashboard/products/ShopProductCardCartlist.js
+++ src/sections/@dashboard/products/ShopProductCardCartlist.js
@@ -1,15 +1,197 @@
+// import PropTypes from 'prop-types';
+// import Button from '@mui/material/Button';
+// import { useContext, useEffect, useState } from 'react';
+// import { Box, Card, Link, Typography, Stack, Radio, FormControlLabel, Rating, IconButton, Alert } from '@mui/material';
+// import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+// import { styled } from '@mui/material/styles';
+// import { fCurrency } from '../../../utils/formatNumber';
+// import { MainContext } from '../../../context/MainContext';
+// import Label from '../../../components/label';
+// import { ColorPreview } from '../../../components/color-utils';
+
+
+
+// const StyledProductImg = styled('img')({
+//   top: 0,
+//   width: '100%',
+//   height: '100%',
+//   objectFit: 'cover',
+//   position: 'absolute',
+// });
+
+// ShopProductCardCartlist.propTypes = {
+//   product: PropTypes.object,
+// };
+
+// export default function ShopProductCardCartlist({ product }) {
+//   const { title, price, category, img, rating, _id } = product;
+//   const { mainState, setMainState } = useContext(MainContext);
+//   const [quantity, setQuantity] = useState(1);
+//   const [showAlert, setShowAlert] = useState(false);
+
+
+//   const handleIconClick = () => {
+//     const updatedCartlist = mainState.cartlist.filter((data) => data.id !== product.id);
+//     setMainState({ ...mainState, cartlist: updatedCartlist });
+//     setShowAlert(true);
+//     console.log("dddddd", updatedCartlist)
+//   };
+
+//   useEffect(() => {
+//     const updateQuntityData = mainState.cartlist;
+//     for (let i = 0; i < updateQuntityData.length; i += 1) {
+//       if (updateQuntityData[i].id === product.id) {
+//         updateQuntityData[i].quantity = quantity;
+//       }
+//     }
+//     setMainState({ ...mainState, cartlist: updateQuntityData })
+//     console.log(mainState.cartlist);
+//   }, [quantity])
+
+//   const handleIncrement = () => {
+//     setQuantity(quantity + 1);
+//   };
+
+//   const handleDecrement = () => {
+//     if (quantity > 1) {
+//       setQuantity(quantity - 1);
+//     }
+//   };
+//   return (
+//     <Card>
+//       <Stack direction="row" spacing={2} sx={{ p: 2 }}>
+
+//         <Stack direction="column" justifyContent="space-between">
+//           <Box
+//             sx={{
+//               width: 100,
+//               height: 100,
+//               position: 'relative',
+//               overflow: 'hidden',
+//               borderRadius: '10%'
+
+//             }}> <StyledProductImg alt={title} src={img} /></Box>
+//         </Stack>
+//         <Stack width="100%" justifyContent="space-between">
+//           <Stack direction="row" alignItems="center">
+
+//             <Link color="inherit" underline="hover" href={`singleProduct/${_id}`}>
+//               <Typography variant="subtitle2" style={{ cursor: 'pointer' }} noWrap>
+//                 {title}
+//               </Typography>
+//             </Link>
+//             <Box flexGrow={1} />
+//             <Button variant="outlined" color="error" onClick={() => handleIconClick()} > <DeleteForeverIcon style={{ cursor: 'pointer', color: '#ed3939' }} /></Button>
+//           </Stack>
+//           <Typography variant="subtitle1">{fCurrency(price)}</Typography>
+
+
+//           <Stack direction="row" alignItems="center">
+
+//             <FormControlLabel
+//               key={rating}
+//               value={rating}
+//               control={
+//                 <Radio
+//                   disableRipple
+//                   color="default"
+//                   icon={<Rating name="half-rating" precision={0.5} readOnly value={rating} />}
+//                   checkedIcon={<Rating name="half-rating" precision={0.5} readOnly value={rating} />}
+//                   sx={{
+//                     '&:hover': { bgcolor: 'transparent' },
+//                   }}
+//                 />
+//               }
+//               sx={{
+//                 my: 0.5,
+//                 borderRadius: 1,
+//                 '&:hover': { opacity: 0.48 },
+//               }}
+//             />
+//             <Box flexGrow={1} />
+//             <Stack direction="row" alignItems="center">
+//               <Button onClick={handleDecrement} variant="outlined">-</Button>
+//               <Button variant="outlined" disabled >{quantity}</Button>
+//               <Button variant="outlined" onClick={handleIncrement}>+</Button>
+//             </Stack>
+//           </Stack>
+//         </Stack>
+
+//       </Stack>
+//     </Card>
+//     //   <Card>
+//     //   <Box sx={{ pt: '100%', position: 'relative' }}>
+//     //   {category && (
+//     //     <Label
+//     //       variant="filled"
+//     //       color={(category === 'shoes' && 'error') || 'info'}
+//     //       sx={{
+//     //         zIndex: 9,
+//     //         top: 16,
+//     //         right: 16,
+//     //         position: 'absolute',
+//     //         textTransform: 'uppercase',
+//     //       }}
+//     //     >
+//     //       {category}
+//     //     </Label>
+//     //   )}
+//     //   <StyledProductImg alt={title} src={img} />
+//     // </Box>
+
+//     // <Stack direction="row" spacing={2} sx={{ p: 3 }}>
+//     //   <Link color="inherit" underline="hover" href={`singleProduct/${_id}`}>
+//     //     <Typography variant="subtitle2" style={{ cursor: 'pointer' }} noWrap>
+//     //       {title}
+//     //     </Typography>
+//     //   </Link>
+
+//     //   <Stack direction="row" alignItems="center" justifyContent="space-between">
+//     //     <Typography variant="subtitle1">{fCurrency(price)}</Typography>
+
+//     //     <Typography>
+//     //       <Stack direction="row" spacing={2}>
+//     //         <DeleteForeverIcon onClick={handleIconClick} style={{ cursor: 'pointer', color: '#ed3939' }} />
+//     //       </Stack>
+//     //     </Typography>
+//     //   </Stack>
+//     //   <Stack>
+//     //     <FormControlLabel
+//     //       key={rating}
+//     //       value={rating}
+//     //       control={
+//     //         <Radio
+//     //           disableRipple
+//     //           color="default"
+//     //           icon={<Rating name="half-rating" precision={0.5} readOnly value={rating} />}
+//     //           checkedIcon={<Rating name="half-rating" precision={0.5} readOnly value={rating} />}
+//     //           sx={{
+//     //             '&:hover': { bgcolor: 'transparent' },
+//     //           }}
+//     //         />
+//     //       }
+//     //       sx={{
+//     //         my: 0.5,
+//     //         borderRadius: 1,
+//     //         '&:hover': { opacity: 0.48 },
+//     //       }}
+//     //     />
+//     //   </Stack>
+//     // </Stack> 
+
+//     //   </Card>
+//   );
+// }
 import PropTypes from 'prop-types';
 import Button from '@mui/material/Button';
 import { useContext, useEffect, useState } from 'react';
-import { Box, Card, Link, Typography, Stack, Radio, FormControlLabel, Rating, IconButton } from '@mui/material';
+import { Box, Card, Link, Typography, Stack, Radio, FormControlLabel, Rating, IconButton, Alert } from '@mui/material';
 import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 import { styled } from '@mui/material/styles';
 import { fCurrency } from '../../../utils/formatNumber';
 import { MainContext } from '../../../context/MainContext';
 import Label from '../../../components/label';
 import { ColorPreview } from '../../../components/color-utils';
-
-
 
 const StyledProductImg = styled('img')({
   top: 0,
@@ -27,12 +209,21 @@ export default function ShopProductCardCartlist({ product }) {
   const { title, price, category, img, rating, _id } = product;
   const { mainState, setMainState } = useContext(MainContext);
   const [quantity, setQuantity] = useState(1);
-
+  const [showAlert, setShowAlert] = useState(false);
 
   const handleIconClick = () => {
+    setShowAlert(true);
+  };
+
+  const handleDeleteProduct = () => {
     const updatedCartlist = mainState.cartlist.filter((data) => data.id !== product.id);
     setMainState({ ...mainState, cartlist: updatedCartlist });
-    console.log("dddddd", updatedCartlist)
+    console.log("dddddd", updatedCartlist);
+    setShowAlert(false);
+  };
+
+  const handleCancelDelete = () => {
+    setShowAlert(false);
   };
 
   useEffect(() => {
@@ -42,9 +233,9 @@ export default function ShopProductCardCartlist({ product }) {
         updateQuntityData[i].quantity = quantity;
       }
     }
-    setMainState({ ...mainState, cartlist: updateQuntityData })
+    setMainState({ ...mainState, cartlist: updateQuntityData });
     console.log(mainState.cartlist);
-  }, [quantity])
+  }, [quantity]);
 
   const handleIncrement = () => {
     setQuantity(quantity + 1);
@@ -55,10 +246,10 @@ export default function ShopProductCardCartlist({ product }) {
       setQuantity(quantity - 1);
     }
   };
+
   return (
     <Card>
       <Stack direction="row" spacing={2} sx={{ p: 2 }}>
-
         <Stack direction="column" justifyContent="space-between">
           <Box
             sx={{
@@ -66,26 +257,26 @@ export default function ShopProductCardCartlist({ product }) {
               height: 100,
               position: 'relative',
               overflow: 'hidden',
-              borderRadius: '10%'
-
-            }}> <StyledProductImg alt={title} src={img} /></Box>
+              borderRadius: '10%',
+            }}
+          >
+            <StyledProductImg alt={title} src={img} />
+          </Box>
         </Stack>
         <Stack width="100%" justifyContent="space-between">
           <Stack direction="row" alignItems="center">
-
             <Link color="inherit" underline="hover" href={`singleProduct/${_id}`}>
               <Typography variant="subtitle2" style={{ cursor: 'pointer' }} noWrap>
                 {title}
               </Typography>
             </Link>
             <Box flexGrow={1} />
-            <Button variant="outlined" color="error" onClick={() => handleIconClick()} > <DeleteForeverIcon style={{ cursor: 'pointer', color: '#ed3939' }} /></Button>
+            <Button variant="outlined" color="error" onClick={handleIconClick}>
+              <DeleteForeverIcon style={{ cursor: 'pointer', color: '#ed3939' }} />
+            </Button>
           </Stack>
           <Typography variant="subtitle1">{fCurrency(price)}</Typography>
-
-
           <Stack direction="row" alignItems="center">
-
             <FormControlLabel
               key={rating}
               value={rating}
@@ -108,75 +299,31 @@ export default function ShopProductCardCartlist({ product }) {
             />
             <Box flexGrow={1} />
             <Stack direction="row" alignItems="center">
-              <Button onClick={handleDecrement} variant="outlined">-</Button>
-              <Button variant="outlined" disabled >{quantity}</Button>
-              <Button variant="outlined" onClick={handleIncrement}>+</Button>
+              <Button onClick={handleDecrement} variant="outlined">
+                -
+              </Button>
+              <Button variant="outlined" disabled>
+                {quantity}
+              </Button>
+              <Button variant="outlined" onClick={handleIncrement}>
+                +
+              </Button>
             </Stack>
           </Stack>
         </Stack>
-
       </Stack>
+      {showAlert && (
+        <Alert severity="warning" sx={{ m: 2 }}>
+          Are you sure you want to delete this product from the cart?
+          <Button variant="outlined" onClick={handleDeleteProduct} sx={{ mx: 2 }}>
+            Delete
+          </Button>
+          <Button variant="outlined" onClick={handleCancelDelete}>
+            Cancel
+          </Button>
+        </Alert>
+      )}
     </Card>
-    //   <Card>
-    //   <Box sx={{ pt: '100%', position: 'relative' }}>
-    //   {category && (
-    //     <Label
-    //       variant="filled"
-    //       color={(category === 'shoes' && 'error') || 'info'}
-    //       sx={{
-    //         zIndex: 9,
-    //         top: 16,
-    //         right: 16,
-    //         position: 'absolute',
-    //         textTransform: 'uppercase',
-    //       }}
-    //     >
-    //       {category}
-    //     </Label>
-    //   )}
-    //   <StyledProductImg alt={title} src={img} />
-    // </Box>
-
-    // <Stack direction="row" spacing={2} sx={{ p: 3 }}>
-    //   <Link color="inherit" underline="hover" href={`singleProduct/${_id}`}>
-    //     <Typography variant="subtitle2" style={{ cursor: 'pointer' }} noWrap>
-    //       {title}
-    //     </Typography>
-    //   </Link>
-
-    //   <Stack direction="row" alignItems="center" justifyContent="space-between">
-    //     <Typography variant="subtitle1">{fCurrency(price)}</Typography>
-
-    //     <Typography>
-    //       <Stack direction="row" spacing={2}>
-    //         <DeleteForeverIcon onClick={handleIconClick} style={{ cursor: 'pointer', color: '#ed3939' }} />
-    //       </Stack>
-    //     </Typography>
-    //   </Stack>
-    //   <Stack>
-    //     <FormControlLabel
-    //       key={rating}
-    //       value={rating}
-    //       control={
-    //         <Radio
-    //           disableRipple
-    //           color="default"
-    //           icon={<Rating name="half-rating" precision={0.5} readOnly value={rating} />}
-    //           checkedIcon={<Rating name="half-rating" precision={0.5} readOnly value={rating} />}
-    //           sx={{
-    //             '&:hover': { bgcolor: 'transparent' },
-    //           }}
-    //         />
-    //       }
-    //       sx={{
-    //         my: 0.5,
-    //         borderRadius: 1,
-    //         '&:hover': { opacity: 0.48 },
-    //       }}
-    //     />
-    //   </Stack>
-    // </Stack> 
-
-    //   </Card>
   );
 }
+
